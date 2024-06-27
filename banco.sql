45548175b8f77f0bad1d257d5e430495bc78732b
Brenda Cristaldo <112977198+brendacristaldo@users.noreply.github.com>
2024-06-27 09:33:15 -0300

    Add files via upload

--- banco.sql
+++ banco.sql
@@ -3,17 +3,20 @@ create database banco;
 use banco;
 
 CREATE TABLE `estabelecimento` (
-  `nome` varchar(200) NOT NULL,
-  `telefone` int NOT NULL,
-  `email` varchar(200) NOT NULL,
-  `endereco` varchar(200) NOT NULL,
-  `senha` varchar(200) NOT NULL
-) ;
+    `nome` VARCHAR(200) NOT NULL,
+    `telefone` VARCHAR(200) NOT NULL,
+    `endereco` VARCHAR(200) NOT NULL,
+    `email` VARCHAR(200) NOT NULL,
+    `senha` VARCHAR(200) NOT NULL,
+    primary key(email)
+);
+
 
 CREATE TABLE `agenda` (
   `nome_cliente` varchar(200) NOT NULL,
   `nome_profissional` varchar(200) NOT NULL,
   `procedimento` varchar(200) NOT NULL,
-  `dia_agendamento` date NOT NULL,
-  `horario` time NOT NULL
-) 
+  `dia_hora` varchar(200) NOT NULL,
+  `estabelecimento_email` varchar(200) NOT NULL,
+  FOREIGN KEY (`estabelecimento_email`) REFERENCES `estabelecimento`(`email`)
+);
